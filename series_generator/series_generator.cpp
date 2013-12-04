45a9949701cdf16c2f1ea5b0c7277c3e160da590
Wang Wei <voidstarting@gmail.com>
2013-12-04 10:26:41 +0800

    update

--- series_generator/series_generator.cpp
+++ series_generator/series_generator.cpp
@@ -1,1 +1,10 @@
-main	
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+int main(int argc, char const *argv[])
+{
+	srand( time(NULL));
+	int num = rand();
+	printf("%d\n", num);
+	return 0;
+}
--- sorting/quicksort/quicksort.cpp
+++ sorting/quicksort/quicksort.cpp
@@ -1,1 +1,5 @@
-
+int main(int argc, char const *argv[])
+{
+	/* code */
+	return 0;
+}
