5795511f324ffa60c493a364d6d94d9b3d0422d3
Wang Wei <voidstarting@gmail.com>
2013-12-04 11:48:55 +0800

    sort

--- sorting/quicksort/quicksort.cpp
+++ sorting/quicksort/quicksort.cpp
@@ -1,5 +1,32 @@
+#include "stdio.h"
+
+
+
+
+int a[] = { 30, 90, 60, 50, 70, 10, 160, 20, 40, 100, 80 };
+
+void print_a()
+{
+	int n = sizeof(a)/sizeof(int);
+
+	printf("a = [");
+	for (int i = 0; i < n; ++i)
+	{
+		if (i>0)
+			printf(",");
+
+		printf("% 3d", a[i]);
+
+	}
+	printf("]\n");
+
+
+
+}
+
+
 int main(int argc, char const *argv[])
 {
-	/* code */
+	print_a();
 	return 0;
 }
