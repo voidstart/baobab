3fabd1ef48f4c0a668910b3510a8029cc6394f48
Wang Wei <voidstarting@gmail.com>
2013-12-04 11:12:09 +0800

    update

--- series_generator/series_generator.cpp
+++ series_generator/series_generator.cpp
@@ -1,10 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-int main(int argc, char const *argv[])
+#include <vector>
+#include <iostream>
+using namespace std;
+//give me N random number, as an ARRAY
+// - memory allocation : fit in RAM, on stack/heap, GC
+// - randomness: not exactly random. 2^32 seed, limited random sea.
+void append_random_ints(vector<int> & v, int n)
 {
 	srand( time(NULL));
-	int num = rand();
-	printf("%d\n", num);
+	for (int i = 0; i < n; ++i)
+	{
+		v.push_back(rand());
+	}
+
+}
+
+int main(int argc, char const *argv[])
+{
+	std::vector<int> v;
+	append_random_ints(v,10);
+
 	return 0;
 }
